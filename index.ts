2569986e523a45a5dbeeff00c7c86b05296159c6
KybalionX <nflorez@ubits.co>
2024-06-22 23:32:16 -0500

    Feat: change lambda response

--- index.ts
+++ index.ts
@@ -1,8 +1,13 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda"
 
-export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
+type LambdaResponse = APIGatewayProxyResultV2 & {
+    event: APIGatewayProxyEvent,
+};
+
+export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<LambdaResponse> => {
     return {
         statusCode: 200,
         body: "Hello there, I was deployed using aws-cli!",
-    }
+        event,
+    };
 }
