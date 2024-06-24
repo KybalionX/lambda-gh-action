40ca1f2c2956f6526d8bf3784eb997b805a01e30
KybalionX <nflorez@ubits.co>
2024-06-23 21:20:34 -0500

    Feat: adding support to array of elements

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import {
-  DynamoDBDocumentClient,
-  PutCommand,
-} from "@aws-sdk/lib-dynamodb";
-const { randomUUID } = require('crypto'); // Added in: node v14.17.0
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+const { randomUUID } = require("crypto"); // Added in: node v14.17.0
 
 const client = new DynamoDBClient({});
 const dynamo = DynamoDBDocumentClient.from(client);
 const tableName = "users-table";
+
+interface BodyFromSqs {
+  name: string;
+  price: number;
+}
 
 export const handler = async (event, context) => {
   let body;
@@ -16,29 +18,28 @@ export const handler = async (event, context) => {
     "Content-Type": "application/json",
   };
 
-  console.log("EVENT: ",event.Records);
+  console.log("EVENT: ", event.Records);
+
+  const eventData = event.Records[0];
 
   try {
-      const storePromises = event.Records.map(record => {
-          let requestJSON = JSON.parse(record.body);
-        return dynamo.send(
-            new PutCommand({
-              TableName: tableName,
-              Item: {
-                id: randomUUID(),
-                price: requestJSON.price,
-                name: requestJSON.name,
-              },
-            })
-          );
-      });
+    const items = JSON.parse(eventData.body) as BodyFromSqs[];
 
-      await Promise.all(storePromises);
-    
-      body = `Stored Items!`;
-    } catch (error) {
-        console.log("Error trying to insert a record in DynamoDB", error);
-    }
+    const storePromises = items.map((record) =>
+      dynamo.send(
+        new PutCommand({
+          TableName: tableName,
+          Item: record,
+        })
+      )
+    );
+
+    await Promise.all(storePromises);
+
+    body = `Stored Items!`;
+  } catch (error) {
+    console.log("Error trying to insert a record in DynamoDB", error);
+  }
 
   return {
     statusCode,
